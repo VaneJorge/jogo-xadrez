696e14bd482fa8c0077786d3e1e5c3dc3d35fd43
VaneJorge <vanjorge.2014@gmail.com>
2020-01-12 15:43:04 -0300

    Colocando peças no tabuleiro

--- src/tabuleiro/Tabuleiro.java
+++ src/tabuleiro/Tabuleiro.java
@@ -3,20 +3,25 @@ package tabuleiro;
 public class Tabuleiro {
 	private int linhas;
 	private int colunas;
-	private Peca[][] peca;
+	private Peca[][] pecas;
 
 	public Tabuleiro(int linhas, int colunas) {
 		this.linhas = linhas;
 		this.colunas = colunas;
-		peca = new Peca[linhas][colunas];
+		pecas = new Peca[linhas][colunas];
 	}
 
 	public Peca peca(int linha, int coluna) {
-		return peca[linha][coluna];
+		return pecas[linha][coluna];
 	}
 
 	public Peca peca(Posicao posicao) {
-		return peca[posicao.getLinha()][posicao.getColuna()];
+		return pecas[posicao.getLinha()][posicao.getColuna()];
+	}
+
+	public void lugarPeca(Peca peca, Posicao posicao) {
+		pecas[posicao.getLinha()][posicao.getColuna()] = peca;
+		peca.posicao = posicao;
 	}
 
 	public int getLinhas() {
--- src/xadrez/PartidaXadrez.java
+++ src/xadrez/PartidaXadrez.java
@@ -1,12 +1,16 @@
 package xadrez;
 
+import tabuleiro.Posicao;
 import tabuleiro.Tabuleiro;
+import xadrez.pecas.Rei;
+import xadrez.pecas.Torre;
 
 public class PartidaXadrez {
 	private Tabuleiro tabuleiro;
 
 	public PartidaXadrez() {
 		tabuleiro = new Tabuleiro(8, 8);
+		setupInicial();
 	}
 
 	public PecaXadrez[][] getPecas() {
@@ -17,5 +21,11 @@ public class PartidaXadrez {
 			}
 		}
 		return mat;
+	}
+
+	private void setupInicial() {
+		tabuleiro.lugarPeca(new Torre(tabuleiro, Cor.WHITE), new Posicao(2, 1));
+		tabuleiro.lugarPeca(new Rei(tabuleiro, Cor.BLACK), new Posicao(0, 4));
+		tabuleiro.lugarPeca(new Rei(tabuleiro, Cor.WHITE), new Posicao(7, 4));
 	}
 }
